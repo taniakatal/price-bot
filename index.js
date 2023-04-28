5bb4d0e8f12ce5848e67b3b4beaea6bd08818e35
taniakatal <130392843+taniakatal@users.noreply.github.com>
2023-04-28 08:19:58 -0700

    jb

--- index.js
+++ index.js
@@ -1,48 +1,92 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const { getCryptoPriceInUSD, generateCryptoChart } = require("./modules/price");
+const { getCryptoPriceInUSD, generateCryptoChart } = require('./modules/price');
 
-const { Telegraf } = require("telegraf");
+const { Telegraf } = require('telegraf');
+const mongoose = require('mongoose');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_API);
-let cryptoSymbol = "bitcoin";
+let cryptoSymbol = 'bitcoin';
 
-bot.on("message", async (ctx) => {
-  try {
-    const { message_id, from, chat, date, text } = ctx.message;
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-    console.log("@" + (from.username || "X") + " - " + chat.id + " - " + text);
+const alertSchema = new mongoose.Schema({
+	symbol: String,
+	price: Number,
+	chatId: Number,
+});
 
-    if (text.startsWith("/")) {
-      const [command, ...args] = text.split(" ");
+const Alert = mongoose.model('Alert', alertSchema);
 
-      if (command === "/start") {
-        ctx.reply(`Hello ${from.first_name}!`);
-      }
+async function checkAlerts() {
+	const symbols = await Alert.distinct('symbol');
 
-      if (command === "/price") {
-        if (args[0]) {
-          cryptoSymbol = args[0].toLowerCase() || cryptoSymbol;
-        }
+	for (const symbol of symbols) {
+		const currentPrice = await getCryptoPriceInUSD(symbol);
+		const alerts = await Alert.find({ symbol, price: { $lte: currentPrice } });
 
-        const price = await getCryptoPriceInUSD(cryptoSymbol);
+		for (const alert of alerts) {
+			bot.telegram.sendMessage(alert.chatId, `🚨 ${symbol.toUpperCase()} reached your price alert of $${alert.price} USD. Current price: $${currentPrice} USD`);
+			await Alert.deleteOne({ _id: alert._id });
+		}
+	}
+}
 
-        ctx.reply(`${cryptoSymbol} is $${price} USD`);
-      }
+setInterval(checkAlerts, 60000);
 
-      if (command === "/chart") {
-        if (args[0]) {
-          cryptoSymbol = args[0].toLowerCase() || cryptoSymbol;
-        }
+bot.on('message', async (ctx) => {
+	try {
+		const { message_id, from, chat, date, text } = ctx.message;
 
-        const imageBuffer = await generateCryptoChart(cryptoSymbol);
+		console.log('@' + (from.username || 'X') + ' - ' + chat.id + ' - ' + text);
 
-        ctx.replyWithPhoto({ source: imageBuffer });
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
+		if (text.startsWith('/')) {
+			const [command, ...args] = text.split(' ');
+
+			if (command === '/start') {
+				ctx.reply(`Hello ${from.first_name}!`);
+			}
+
+			if (command === '/price') {
+				if (args[0]) {
+					cryptoSymbol = args[0].toLowerCase() || cryptoSymbol;
+				}
+
+				const price = await getCryptoPriceInUSD(cryptoSymbol);
+
+				ctx.reply(`${cryptoSymbol} is $${price} USD`);
+			}
+
+			if (command === '/chart') {
+				if (args[0]) {
+					cryptoSymbol = args[0].toLowerCase() || cryptoSymbol;
+				}
+
+				const imageBuffer = await generateCryptoChart(cryptoSymbol);
+
+				ctx.replyWithPhoto({ source: imageBuffer });
+			}
+
+			if (command === '/alert') {
+				if (args.length >= 2) {
+					const symbol = args[0].toLowerCase();
+					const price = parseFloat(args[1]);
+
+					const alert = new Alert({ symbol, price, chatId: chat.id });
+					await alert.save();
+
+					ctx.reply(`Price alert set for ${symbol.toUpperCase()} at $${price} USD`);
+				} else {
+					ctx.reply('Please use the following format: /alert SYMBOL PRICE');
+				}
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 bot.launch();
--- modules/price.js
+++ modules/price.js
@@ -1,46 +1,103 @@
-const axios = require('axios');
-const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
+const axios = require("axios");
+const { ChartJSNodeCanvas } = require("chartjs-node-canvas");
 const canvas = new ChartJSNodeCanvas({ width: 1000, height: 1000 });
 
 function getCryptoPriceInUSD(cryptoSymbol) {
-	return axios
-		.get(`https://api.coingecko.com/api/v3/simple/price?ids=${cryptoSymbol}&vs_currencies=usd`)
-		.then((response) => {
-			const price = response.data[cryptoSymbol].usd;
+  return axios
+    .get(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${cryptoSymbol}&vs_currencies=usd`
+    )
+    .then((response) => {
+      const price = response.data[cryptoSymbol].usd;
 
-			return price;
-		})
-		.catch((error) => {
-			console.log(`Error fetching price data: ${error}`);
-		});
+      return price;
+    })
+    .catch((error) => {
+      console.log(`Error fetching price data: ${error}`);
+    });
 }
 
 async function generateCryptoChart(cryptoSymbol) {
-	const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${cryptoSymbol}/market_chart?vs_currency=usd&days=7`);
-	const prices = response.data.prices;
+  const response = await axios.get(
+    `https://api.coingecko.com/api/v3/coins/${cryptoSymbol}/market_chart?vs_currency=usd&days=7`
+  );
+  const prices = response.data.prices;
 
-	const dates = prices.map((price) => new Date(price[0]));
-	const pricesInUSD = prices.map((price) => price[1]);
+  const dates = prices.map((price) => new Date(price[0]));
+  const pricesInUSD = prices.map((price) => price[1]);
 
-	const chartData = {
-		type: 'line',
-		data: {
-			labels: dates,
-			datasets: [
-				{
-					label: `TANIABOT - ${cryptoSymbol.toUpperCase()} Price (USD)`,
-					data: pricesInUSD,
-					borderColor: 'blue',
-					fill: false,
-				},
-			],
-		},
-	};
+  const chartData = {
+    type: "line",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: `TANIABOT - ${cryptoSymbol.toUpperCase()} Price (USD)`,
+          data: pricesInUSD,
+          borderColor: "blue",
+          fill: false,
+        },
+      ],
+    },
+  };
 
-	const imageBuffer = await canvas.renderToBuffer(chartData);
+  const imageBuffer = await canvas.renderToBuffer(chartData);
 
-	return imageBuffer;
+  return imageBuffer;
 }
+
+// Function to fetch cryptocurrency information from CoinGecko API
+function getCryptocurrencyInfo(coinId) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/${coinId}`
+      );
+      resolve(response.data);
+    } catch (error) {
+      console.error(error);
+      reject(
+        new Error(
+          "Failed to fetch cryptocurrency data. Please check the coin ID and try again."
+        )
+      );
+    }
+  });
+}
+
+function getTrendingcrypto() {
+  return axios
+    .get(`https://api.coingecko.com/api/v3/search/trending`)
+    .then((response) => {
+      const trending = response.data.coins;
+
+      return trending;
+    })
+    .catch((error) => {
+      console.log(`Error fetching price data: ${error}`);
+    });
+}
+
+async function fetchPublicTreasuryInfo(coinId) {
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/companies/public_treasury/${coinId}`
+    );
+    const publicTreasuryInfo = response.data;
+    return publicTreasuryInfo;
+  } catch (error) {
+    console.error(
+      `Failed to fetch public treasury information for ${coinId}:`,
+      errors
+    );
+    return null;
+  }
+}
+exports.fetchPublicTreasuryInfo = fetchPublicTreasuryInfo;
+
+exports.getTrendingcrypto = getTrendingcrypto;
+
+exports.getCryptocurrencyInfo = getCryptocurrencyInfo;
 
 exports.generateCryptoChart = generateCryptoChart;
 
