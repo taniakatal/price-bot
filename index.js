cc5e699d76c5e4e0ee7e3f469deff1ac5c072201
taniakatal <130392843+taniakatal@users.noreply.github.com>
2023-04-28 10:03:52 -0700

    v 1

--- index.js
+++ index.js
@@ -1,236 +1,137 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const { getCryptoPriceInUSD, generateCryptoChart } = require("./modules/price");
+const { getCryptoPriceInUSD, generateCryptoChart } = require('./modules/price');
 
-const { Telegraf } = require("telegraf");
-const mongoose = require("mongoose");
+const { Telegraf } = require('telegraf');
+const mongoose = require('mongoose');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_API);
-let cryptoSymbol = "bitcoin";
+let cryptoSymbol = 'bitcoin';
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 const alertSchema = new mongoose.Schema({
   symbol: String,
   price: Number,
-  type: { type: String, enum: ["above", "below"] },
+  type: { type: String, enum: ['above', 'below'] },
   chatId: Number,
 });
 
-const Alert = mongoose.model("Alert", alertSchema);
+const Alert = mongoose.model('Alert', alertSchema);
 
-const priceSchema = new mongoose.Schema(
-  {
-    chatId: Number,
-    symbol: String,
-    lastPrice: Number,
-  },
-  { timestamps: { createdAt: "createdAt" }, expireAfterSeconds: 24 * 60 * 60 }
-);
 
-const Price = mongoose.model("Price", priceSchema);
-
-async function getPriceChange(chatId, symbol, currentPrice) {
-  const priceData = await Price.findOne({ chatId, symbol });
-
-  if (priceData) {
-    const lastPrice = priceData.lastPrice;
-    const priceChange = currentPrice - lastPrice;
-    const priceChangePercent = (currentPrice / lastPrice - 1) * 100;
-    const priceChangeText =
-      priceChange > 0 ? `+${priceChange.toFixed(2)}` : priceChange.toFixed(2);
-    const priceChangePercentText =
-      priceChangePercent > 0
-        ? `+${priceChangePercent.toFixed(2)}%`
-        : `${priceChangePercent.toFixed(2)}%`;
-
-    if (priceChangeText) {
-      return `Price change since last check: ${priceChangeText} USD (${priceChangePercentText})`;
-    } else {
-      return "";
-    }
-  } else {
-    return "";
-  }
-}
 
 async function checkAlerts() {
-  console.log("Running Check ALerts ");
-
-  const symbols = await Alert.distinct("symbol");
-
+  const symbols = await Alert.distinct('symbol');
+  
   for (const symbol of symbols) {
     const currentPrice = await getCryptoPriceInUSD(symbol);
-    const alertsAbove = await Alert.find({
-      symbol,
-      type: "above",
-      price: { $lte: currentPrice },
-    });
-    const alertsBelow = await Alert.find({
-      symbol,
-      type: "below",
-      price: { $gte: currentPrice },
-    });
-
+    const alertsAbove = await Alert.find({ symbol, type: 'above', price: { $lte: currentPrice } });
+    const alertsBelow = await Alert.find({ symbol, type: 'below', price: { $gte: currentPrice } });
+    
     for (const alert of [...alertsAbove, ...alertsBelow]) {
-      const typeText =
-        alert.type === "above" ? "reached or exceeded" : "fell or reached";
-      bot.telegram.sendMessage(
-        alert.chatId,
-        `🚨 ${symbol.toUpperCase()} ${typeText} your price alert of $${
-          alert.price
-        } USD. Current price: $${currentPrice} USD`
-      );
+      const typeText = alert.type === 'above' ? 'reached or exceeded' : 'fell or reached';
+      bot.telegram.sendMessage(alert.chatId, `🚨 ${symbol.toUpperCase()} ${typeText} your price alert of $${alert.price} USD. Current price: $${currentPrice} USD`);
       await Alert.deleteOne({ _id: alert._id });
-    }
-
-    const priceData = await Price.findOneAndUpdate(
-      { chatId: chatId },
-      { lastPrice: currentPrice },
-      { upsert: true, new: true }
-    );
-
-    if (priceData) {
-      const priceChangeText = await getPriceChange(
-        chatId,
-        symbol,
-        currentPrice
-      );
-
-      if (priceChangeText) {
-        bot.telegram.sendMessage(
-          alert.chatId,
-          `${symbol.toUpperCase()} is $${currentPrice} USD. 
-          ${priceChangeText}`
-        );
-      }
     }
   }
 }
 
-setInterval(checkAlerts, 33 * 1000);
 
-bot.on("message", async (ctx) => {
-  const { message_id, from, chat, date, text } = ctx.message;
+setInterval(checkAlerts, 60000);
 
-  console.log("@" + (from.username || "X") + " - " + chat.id + " - " + text);
+bot.on('message', async (ctx) => {
+	try {
+		const { message_id, from, chat, date, text } = ctx.message;
 
-  try {
-    if (text.startsWith("/")) {
-      const [command, ...args] = text.split(" ");
+		console.log('@' + (from.username || 'X') + ' - ' + chat.id + ' - ' + text);
 
-      if (command === "/start") {
-        const welcomeMessage = `Hello ${from.first_name}! Here are the available commands:
-  
-  /price [SYMBOL] - Get the current price of the specified cryptocurrency in USD.
-  /chart [SYMBOL] - Get a chart of the specified cryptocurrency's price history.
-  /alert SYMBOL PRICE - Set a price alert for the specified cryptocurrency when it reaches or falls below the given price in USD.
-  /viewalerts - View all your active price alerts.
-  
-  Replace [SYMBOL] with the cryptocurrency symbol, like BTC for Bitcoin or ETH for Ethereum. If no symbol is provided, the default is Bitcoin.`;
-        ctx.reply(welcomeMessage);
-      }
+		if (text.startsWith('/')) {
+			const [command, ...args] = text.split(' ');
 
-      if (command === "/price") {
-        if (args[0]) {
-          cryptoSymbol = args[0].toLowerCase();
-        }
+			if (command === '/start') {
+				const welcomeMessage = `Hello ${from.first_name}! Here are the available commands:
+/price [SYMBOL] - Get the current price of the specified cryptocurrency in USD.
+/chart [SYMBOL] - Get a chart of the specified cryptocurrency's price history.
+/alert SYMBOL PRICE - Set a price alert for the specified cryptocurrency when it reaches or falls below the given price in USD.
+/viewalerts - View all your active price alerts.
+Replace [SYMBOL] with the cryptocurrency symbol, like BTC for Bitcoin or ETH for Ethereum. If no symbol is provided, the default is Bitcoin.`;
+				ctx.reply(welcomeMessage);
+			}
 
-        const currentPrice = await getCryptoPriceInUSD(cryptoSymbol);
-        const priceChangeText = await getPriceChange(
-          chat.id,
-          cryptoSymbol,
-          currentPrice
-        );
+			if (command === '/price') {
+				if (args[0]) {
+					cryptoSymbol = args[0].toLowerCase() || cryptoSymbol;
+				}
 
-        const priceData = new Price({
-          chatId: chat.id,
-          symbol: cryptoSymbol,
-          lastPrice: currentPrice,
-        });
+				const price = await getCryptoPriceInUSD(cryptoSymbol);
 
-        await priceData.save();
+				ctx.reply(`${cryptoSymbol} is $${price} USD`);
+			}
 
-        ctx.reply(
-          `${cryptoSymbol.toUpperCase()} is $${currentPrice} USD. ${priceChangeText}`
-        );
-      }
+			if (command === '/chart') {
+				if (args[0]) {
+					cryptoSymbol = args[0].toLowerCase() || cryptoSymbol;
+				}
 
-      if (command === "/chart") {
-        if (args[0]) {
-          cryptoSymbol = args[0].toLowerCase() || cryptoSymbol;
-        }
+				const imageBuffer = await generateCryptoChart(cryptoSymbol);
 
-        const imageBuffer = await generateCryptoChart(cryptoSymbol);
+				ctx.replyWithPhoto({ source: imageBuffer });
+			}
 
-        ctx.replyWithPhoto({ source: imageBuffer });
-      }
-
-      if (command === "/alert") {
-        if (args.length >= 3) {
-          const symbol = args[0].toLowerCase();
-          const price = parseFloat(args[1]);
-          const type = args[2].toLowerCase() === "above" ? "above" : "below";
-
-          const alert = new Alert({ symbol, price, type, chatId: chat.id });
-          await alert.save();
-
-          ctx.reply(
-            `Price alert set for ${symbol.toUpperCase()} ${
-              type === "above" ? "above" : "below"
-            } $${price} USD`
-          );
-        } else {
-          ctx.reply(
-            "Please use the following format: /alert SYMBOL PRICE ABOVE/BELOW"
-          );
-        }
-      }
-
-      if (command === "/viewalerts") {
-        const alerts = await Alert.find({ chatId: chat.id });
-
-        if (alerts.length > 0) {
-          let alertMessage = "Your active alerts:\n\n";
-          alerts.forEach((alert) => {
-            alertMessage += `ID: ${
-              alert._id
-            } 🔔 ${alert.symbol.toUpperCase()} at $${alert.price} USD\n`;
-          });
-          ctx.reply(alertMessage);
-        } else {
-          ctx.reply("You have no active alerts.");
-        }
-      }
-
-      if (command === "/cancelalert") {
-        if (args[0]) {
-          const alertId = args[0];
-          const alert = await Alert.findOne({ _id: alertId, chatId: chat.id });
-
-          if (alert) {
-            await Alert.deleteOne({ _id: alertId });
-            ctx.reply(
-              `Alert for ${alert.symbol.toUpperCase()} at $${
-                alert.price
-              } USD has been cancelled.`
-            );
-          } else {
-            ctx.reply(
-              "Invalid alert ID or the alert does not belong to this chat."
-            );
-          }
-        } else {
-          ctx.reply("Please use the following format: /cancelalert ALERT_ID");
-        }
-      }
-    }
-  } catch (error) {
-    console.log(error);
+if (command === '/alert') {
+  if (args.length >= 3) {
+    const symbol = args[0].toLowerCase();
+    const price = parseFloat(args[1]);
+    const type = args[2].toLowerCase() === 'above' ? 'above' : 'below';
+    
+    const alert = new Alert({ symbol, price, type, chatId: chat.id });
+    await alert.save();
+    
+    ctx.reply(`Price alert set for ${symbol.toUpperCase()} ${type === 'above' ? 'above' : 'below'} $${price} USD`);
+  } else {
+    ctx.reply('Please use the following format: /alert SYMBOL PRICE ABOVE/BELOW');
   }
+}
+
+
+
+			if (command === '/viewalerts') {
+				const alerts = await Alert.find({ chatId: chat.id });
+
+				if (alerts.length > 0) {
+					let alertMessage = 'Your active alerts:\n\n';
+					alerts.forEach((alert) => {
+						alertMessage += `ID: ${alert._id} 🔔 ${alert.symbol.toUpperCase()} at $${alert.price} USD\n`;
+					});
+					ctx.reply(alertMessage);
+				} else {
+					ctx.reply('You have no active alerts.');
+				}
+			}
+
+			if (command === '/cancelalert') {
+				if (args[0]) {
+					const alertId = args[0];
+					const alert = await Alert.findOne({ _id: alertId, chatId: chat.id });
+
+					if (alert) {
+						await Alert.deleteOne({ _id: alertId });
+						ctx.reply(`Alert for ${alert.symbol.toUpperCase()} at $${alert.price} USD has been cancelled.`);
+					} else {
+						ctx.reply('Invalid alert ID or the alert does not belong to this chat.');
+					}
+				} else {
+					ctx.reply('Please use the following format: /cancelalert ALERT_ID');
+				}
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 bot.launch();
